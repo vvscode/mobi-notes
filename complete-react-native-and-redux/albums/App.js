46b4cbd316b5844f629a96986d7e2af4bceded55
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-09 16:16:42 +0300

    Add simple text

--- complete-react-native-and-redux/albums/App.js
+++ complete-react-native-and-redux/albums/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { AppRegistry, Text } from 'react-native';
 
-const App = () => (
+const App = () => (<Text>
   <Text>Hello there from linked file</Text>
+  <Text>Hello there from linked file</Text>
+
+  <Text>Hello there from linked file</Text>
+
+  <Text>Hello there from linked file</Text>
+
+  </Text>
 );
 
 AppRegistry.registerComponent('albums', () => App);
