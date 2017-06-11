03595fe8c59bee142f95501872b535729df49b0f
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-11 14:54:07 +0300

    Base styling for components

--- complete-react-native-and-redux/albums/src/components/header.js
+++ complete-react-native-and-redux/albums/src/components/header.js
@@ -1,11 +1,24 @@
 // Import libs
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  Text
+} from 'react-native';
 
 // Make a component
-const Header = () => (
-  <Text>Albums!</Text>
-);
+const Header = () => {
+  const {
+    textStyle
+  } = styles;
+  return (<Text style={textStyle} > Albums! < /Text>);
+};
+
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
+
 
 // Make the component availbale from other parts
 export default Header;
