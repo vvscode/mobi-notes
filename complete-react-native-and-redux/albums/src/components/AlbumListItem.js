d839bd1c47ac77703e75663b1fb2616688c2033e
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-13 14:42:45 +0300

    Use Linking to open page to order

--- complete-react-native-and-redux/albums/src/components/AlbumListItem.js
+++ complete-react-native-and-redux/albums/src/components/AlbumListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -28,7 +28,7 @@ const AlbumsListItem = ({ title, artist, url, image, thumbnail_image }) => (
       </CardSection>
       <CardSection>
         <Button
-          onPress={() => console.log(`Pressed ${title} button`)}
+          onPress={() => Linking.openURL(url)}
         >Click</Button>
       </CardSection>
   </Card>
