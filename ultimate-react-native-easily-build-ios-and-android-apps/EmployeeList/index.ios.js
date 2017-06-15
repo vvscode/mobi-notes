6ae92cbeb0dd7c662f8943b134775a952500d848
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-15 16:52:20 +0300

    Generate EmployeeList app

---
+++ ultimate-react-native-easily-build-ios-and-android-apps/EmployeeList/index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import EmployeeMaster from './app/EmployeeMaster';
+
+export default class EmployeeList extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <EmployeeMaster />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('EmployeeList', () => EmployeeList);
