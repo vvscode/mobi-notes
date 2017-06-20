fa151a5b2ea5312f00d5825ab06c5005a77258dd
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-20 19:51:15 +0300

    Add TextInput

--- complete-react-native-and-redux/auth/src/components/LoginForm.js
+++ complete-react-native-and-redux/auth/src/components/LoginForm.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+  
   render() {
+    const { email, password } = this.state;
     return (<Card>
-      <CardSection></CardSection>
-      <CardSection></CardSection>
       <CardSection>
-        <Button>Log in</Button>
+        <TextInput 
+        keyboardType="email-address"
+        placeholder="Email"
+        value={email}
+        maxLength={20}
+        onChangeText={(email) => this.setState({email})}
+        style={{height: 20, width: 100 }}
+        />
+      </CardSection>
+      <CardSection>
+        <TextInput 
+        keyboardType="default"
+        placeholder="Password"
+        value={password}
+        maxLength={20}
+        secureTextEntry={true}
+        onChangeText={(password) => this.setState({password})}
+        style={{height: 20, width: 100 }}
+        />
+      </CardSection>
+      <CardSection>
+        <Button>Log in ({email} / {password})</Button>
       </CardSection>
       
     </Card>);
