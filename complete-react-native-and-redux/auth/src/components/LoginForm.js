1cfb5791039e1d39353db85aa7934ba561222d09
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-21 20:03:15 +0300

    Signin logic

--- complete-react-native-and-redux/auth/src/components/LoginForm.js
+++ complete-react-native-and-redux/auth/src/components/LoginForm.js
@@ -1,15 +1,49 @@
 import React, {Component} from 'react';
-import {View, TextInput} from 'react-native';
+import {View, TextInput, Text} from 'react-native';
+import firebase from 'firebase';
+
 import {Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: null
+  }
+
+  onAuthSuccess() {
+    this.setState({ error: null });
+    console.log('onAuthSuccess');
+  }
+
+  onAuthError(err) {
+    console.log('onAuthError', err);
+    this.setState({
+      error: 'Something went wrong'
+    })
+  }
+
+  onButtonPress() {
+    const {email, password} = this.state;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => this.onAuthSuccess())
+      .catch(() => firebase.auth().createUserWithEmailAndPassword(email, password))
+      .catch((err) => this.onAuthError(err));
+  }
+
+  renderErrorMsg() {
+    return this.state.error ? (
+      <CardSection style={styles.errorWrapperStyles}>
+        <Text style={styles.errorStyles}>{this.state.error}</Text>
+      </CardSection>
+    ) : null;
   }
 
   render() {
-    const {email, password} = this.state;
+    const {email, password, error} = this.state;
     return (
       <Card>
         <CardSection>
@@ -30,14 +64,28 @@ class LoginForm extends Component {
             secureTextEntry={true}
             onChangeText={(password) => this.setState({password})}/>
         </CardSection>
+
         <CardSection>
-          <Button>Log in ({email}
-            / {password})</Button>
+          <Button onPress={this
+            .onButtonPress
+            .bind(this)}>Log in</Button>
         </CardSection>
+        {error && this.renderErrorMsg()}
 
       </Card>
     );
   }
 }
+
+const styles = {
+  errorStyles: {
+    color: 'red',
+    fontSize: 18,
+    flex: 1,
+    textAlign: 'center'
+  },
+  errorWrapperStyles: {
+  }
+};
 
 export default LoginForm;
