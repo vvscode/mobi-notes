32ab0c4a8c1a54e9aa8512f655a96813eec1b35e
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-23 13:30:08 +0300

    Use header

--- complete-react-native-and-redux/tech_stack/src/App.js
+++ complete-react-native-and-redux/tech_stack/src/App.js
@@ -4,11 +4,14 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
 import reducers from './reducers';
+import {Header} from './components/common/index'
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View/>
+      <View>
+        <Header text="Tech Stack"/>
+      </View>
     </Provider>
   );
 };
