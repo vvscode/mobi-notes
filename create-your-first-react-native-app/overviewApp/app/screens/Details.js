a850ffec752f4bab3c3730e988d5484098af77da
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-25 21:48:00 +0300

    Display some data at details screen

--- create-your-first-react-native-app/overviewApp/app/screens/Details.js
+++ create-your-first-react-native-app/overviewApp/app/screens/Details.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
+import {capitalizeFirstLetter} from '../helpers/string';
+
 class Details extends Component {
   render() {
+    const {navigation} = this.props;
+    const {contact} = navigation.state.params;
+    const {name} = contact;
+
+    const contactName = `${capitalizeFirstLetter(name.first)} ${capitalizeFirstLetter(name.last)}`
+
     return (
       <View style={styles.viewStyle}>
-        <Text>Details Screen</Text>
+        <Text>Details Screen for {contactName}</Text>
       </View>
     )
   }
