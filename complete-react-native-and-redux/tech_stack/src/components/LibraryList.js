21445bd4a895de3e64f175ee7c26dd965c3cc07d
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-06-27 08:38:22 +0300

    Use ListView for displaying list

--- complete-react-native-and-redux/tech_stack/src/components/LibraryList.js
+++ complete-react-native-and-redux/tech_stack/src/components/LibraryList.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Text, View, ListView} from 'react-native';
+import {connect} from 'react-redux';
 
 class LibraryList extends Component {
-  render() {
-    const { libraries = [] }  = this.props;
-    const items = libraries.map(({id, title, description}) => (
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow({id, title, description}) {
+    return (
       <Text>#{id} {title}</Text>
-    ));
-    return (<View>{items}</View>);
+    );
+  }
+
+  render() {
+    return (<ListView
+      dataSource={this.dataSource}
+      renderRow={this
+      .renderRow
+      .bind(this)}/>);
   }
 }
 
 const mapStateToProps = ({libraries}) => ({libraries});
 
-export default connect(
-  mapStateToProps,  
-)(LibraryList);
+export default connect(mapStateToProps,)(LibraryList);
