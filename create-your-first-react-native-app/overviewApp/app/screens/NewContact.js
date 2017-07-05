1dbd040325b113183d64341af446c4514058f5ed
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-07-05 21:28:40 +0300

    Create form from config

--- create-your-first-react-native-app/overviewApp/app/screens/NewContact.js
+++ create-your-first-react-native-app/overviewApp/app/screens/NewContact.js
@@ -1,15 +1,68 @@
 import React, {Component} from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, View} from 'react-native';
 
 import colors from '../config/colors';
 import {TextInput} from '../components/TextInput';
+import {PrimaryButton} from '../components/Buttons';
+
+const fields = [
+  {
+    placeholder: 'First Name',
+    stateKey: 'firstName'
+  }, {
+    placeholder: 'Last Name',
+    stateKey: 'lastName'
+  }, {
+    placeholder: 'Email',
+    stateKey: 'email'
+  }, {
+    placeholder: 'Mobile Phone',
+    stateKey: 'mobilePhone'
+  }, {
+    placeholder: 'City',
+    stateKey: 'city'
+  }, {
+    placeholder: 'birthdate',
+    stateKey: 'birthdate'
+  }, {
+    placeholder: 'registered',
+    stateKey: 'registered'
+  }, {
+    placeholder: 'User Name',
+    stateKey: 'userName'
+  }
+];
 
 class NewContact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  onInputChange = (text, stateKey) => {
+    const mod = {};
+    mod[stateKey] = text;
+    this.setState(mode);
+  }
+
+  handleSubmit = () => {
+    alert('Submit')
+  }
+
   render() {
     return (
       <ScrollView style={styles.viewStyle}>
-        <TextInput placeholder="First Name..."/>
-        <TextInput placeholder="Last Name..."/>
+        {fields.map((item) => (<TextInput
+          placeholder={item.placeholder}
+          onChangeText={(text) => this.onInputChange(text, item.stateKey)}
+          key={item.stateKey}/>))}
+        <View>
+          <PrimaryButton 
+            label="Create"
+            onPress={() => this.handleSubmit()}
+            />
+        </View>
       </ScrollView>
     )
   }
