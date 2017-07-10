208ad71f62c1e9d0193607a860fe3a58f8527dfb
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-07-10 13:22:27 +0300

    Add handlers inside LoginForm

--- complete-react-native-and-redux/manager/src/components/LoginForm.js
+++ complete-react-native-and-redux/manager/src/components/LoginForm.js
@@ -3,13 +3,35 @@ import React, {Component} from 'react';
 import {Card, CardSection, Input, Button} from './common';
 
 export class LoginForm extends Component {
+  onEmailChange() {}
+
+  onPasswordChange() {}
+
   render() {
     return (
       <Card>
-        <CardSection><Input label="Email" placeholder="email@at.com"/></CardSection>
-        <CardSection><Input label="Password" placeholder="password"/></CardSection>
         <CardSection>
-          <Button>Login</Button>
+          <Input
+            label="Email"
+            placeholder="email@at.com"
+            onChangeText={this
+            .onEmailChange
+            .bind(this)}/>
+        </CardSection>
+        <CardSection>
+          <Input
+            label="Password"
+            placeholder="password"
+            onChangeText={this
+            .onPasswordChange
+            .bind(this)}/>
+        </CardSection>
+        <CardSection>
+          <Button onPress={this
+            .onSubmit
+            .bind(this)}>
+            Login
+          </Button>
         </CardSection>
       </Card>
     );
