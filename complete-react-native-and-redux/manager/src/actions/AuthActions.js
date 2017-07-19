275afbb26aac7d1214ef2e9e38085cc6fb161f40
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-07-19 22:09:57 +0300

    Small refactoring for actions

---
+++ complete-react-native-and-redux/manager/src/actions/AuthActions.js
@@ -0,0 +1,17 @@
+import * as types from './types';
+
+export const emailChanged = (text) => ({type: types.EMAIL_CHANGED, payload: text});
+
+export const passwordChanged = (text) => ({type: types.PASSWORD_CHANGED, payload: text});
+
+export const loginFormSubmit = ({email, password}) => (dispatch) => {
+  dispatch({type: types.LOGIN_USER_START});
+  let firebaseAuth = firebase.auth();
+  Promise
+    .resolve()
+    .then(() => firebaseAuth.signInWithEmailAndPassword(email, password))
+    .catch(() => firebaseAuth.createUserWithEmailAndPassword(email, password))
+    .then((user) => dispatch({type: types.LOGIN_USER_SUCCESS, payload: user}))
+    .then(() => Actions.main())
+    .catch(({message}) => dispatch({type: types.LOGIN_USER_FAIL, payload: message}));
+};
--- complete-react-native-and-redux/manager/src/actions/index.js
+++ complete-react-native-and-redux/manager/src/actions/index.js
@@ -3,20 +3,7 @@ import { Actions } from 'react-native-router-flux';
 
 import * as types from './types';
 
-export const emailChanged = (text) => ({type: types.EMAIL_CHANGED, payload: text});
 
-export const passwordChanged = (text) => ({type: types.PASSWORD_CHANGED, payload: text});
-
-export const loginFormSubmit = ({email, password}) => (dispatch) => {
-  dispatch({type: types.LOGIN_USER_START});
-  let firebaseAuth = firebase.auth();
-  Promise
-    .resolve()
-    .then(() => firebaseAuth.signInWithEmailAndPassword(email, password))
-    .catch(() => firebaseAuth.createUserWithEmailAndPassword(email, password))
-    .then((user) => dispatch({type: types.LOGIN_USER_SUCCESS, payload: user}))
-    .then(() => Actions.main())
-    .catch(({message}) => dispatch({type: types.LOGIN_USER_FAIL, payload: message}));
-};
 
 export * from './EmployeeActions';
+export * from './AuthActions';
