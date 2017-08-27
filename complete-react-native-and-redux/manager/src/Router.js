2321d86525668fd8094317f863726d484e8029e3
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-08-27 19:19:09 +0300

    Navigate to creat employee form

--- complete-react-native-and-redux/manager/src/Router.js
+++ complete-react-native-and-redux/manager/src/Router.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreateForm from './components/EmployeeCreateForm';
 
 const RouterComponent = () => (
-  <Router sceneStyle={{ paddingTop: 65 }}>
+  <Router sceneStyle={{
+    paddingTop: 65
+  }}>
     <Scene key="auth">
       <Scene key="login" component={LoginForm} title="Please Login"/>
     </Scene>
     <Scene key="main" initial={true}>
-      <Scene 
-        key="employeeList" 
-        component={EmployeeList} 
-        title="Employee List" 
-        rightTitle="Add" 
-        onRight={() => alert('add')}
+      <Scene
+        key="employeeList"
+        component={EmployeeList}
+        title="Employee List"
+        rightTitle="Add"
+        onRight={() => Actions.employeeCreateForm()}/>
+      <Scene
+        key="employeeCreateForm"
+        component={EmployeeCreateForm}
+        title="Employee List"
         />
     </Scene>
   </Router>
---
+++ complete-react-native-and-redux/manager/src/components/EmployeeCreateForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class EmployeeCreateForm extends Component {
+  render () {
+    return (
+      <View><Text>Create employee</Text></View>
+    )
+  }
+}
+
+export default EmployeeCreateForm;
