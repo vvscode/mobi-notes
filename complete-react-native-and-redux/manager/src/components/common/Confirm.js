c24421bcb32e599dac6eef052fc7d150633843b3
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-08-30 18:25:40 +0300

    Implement Confirm component

--- complete-react-native-and-redux/manager/src/components/common/Confirm.js
+++ complete-react-native-and-redux/manager/src/components/common/Confirm.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Text, View, Modal } from 'react-native';
+import {Text, View, Modal} from 'react-native';
 
-import { CardSection } from './CardSection';
-import { Button } from './Button';
+import {CardSection} from './CardSection';
+import {Button} from './Button';
 
-const Confirm = () => {
-  
+const Confirm = ({
+  children,
+  onAccept = () => null,
+  onDecline = () => null,
+  visible
+}) => {
+  return (
+    <Modal
+      animationType="slide"
+      onRequestClose={() => {}}
+      transparent
+      visible={visible}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  )
 }
 
-export { Confirm };
+export {Confirm};
