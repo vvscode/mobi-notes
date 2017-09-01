bc28e2b074200aae110b3cd49631389522e8d2f2
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-09-01 19:13:00 +0300

    Implement Confirm styles

--- complete-react-native-and-redux/manager/src/components/common/Confirm.js
+++ complete-react-native-and-redux/manager/src/components/common/Confirm.js
@@ -10,16 +10,16 @@ const Confirm = ({
   onDecline = () => null,
   visible
 }) => {
+  const {containerStyle, textStyle, cardSectionStyle} = styles;
   return (
     <Modal
-      animationType="slide"
+      animationType={"slide"}
       onRequestClose={() => {}}
       transparent
-      visible={visible}
-    >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      visible={visible}>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
         </CardSection>
         <CardSection>
           <Button onPress={onAccept}>Yes</Button>
@@ -30,4 +30,22 @@ const Confirm = ({
   )
 }
 
-export {Confirm};
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    lineHeight: 40,
+    textAlign: 'center'
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    position: 'relative',
+    justifyContent: 'center',
+    flex: 1
+  }
+}
+
+export { Confirm };
