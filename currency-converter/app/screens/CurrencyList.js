561f7d58cd8c13a2ade65ce06ea6103b4ee00992
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-09-12 23:22:33 +0300

    Currency List Screen: Actions

--- currency-converter/app/screens/CurrencyList.js
+++ currency-converter/app/screens/CurrencyList.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { FlatList, Text, StatusBar, View } from 'react-native';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { FlatList, StatusBar, View } from 'react-native';
+import { connect } from 'react-redux';
 
-import currencies from '../data/currencies';
 import { ListItem, Separator } from '../components/List';
+import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
-const TEMP_CURRENT_CURRENCY = 'CAD';
+const TEMP_CURRENT_CURRENCY = 'USD';
 
 class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
-  }
+    dispatch: PropTypes.func,
+  };
 
-  handlePress = (item) => {
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
+    }
+
     this.props.navigation.goBack(null);
   };
 
@@ -22,17 +32,19 @@ class CurrencyList extends Component {
         <StatusBar translucent={false} barStyle="default" />
         <FlatList
           data={currencies}
-          renderItem={({ item }) => <ListItem
-            text={item}
-            selected={item === TEMP_CURRENT_CURRENCY}
-            onPress={this.handlePress}
-          />}
-          ItemSeparatorComponent={Separator}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={() => this.handlePress(item)}
+            />
+          )}
           keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
         />
       </View>
     );
   }
 }
 
-export default CurrencyList;
+export default connect()(CurrencyList);
