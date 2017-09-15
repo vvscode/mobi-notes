c88d17f815bca4afdcee22e0956a7d48222e240b
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-09-15 22:51:53 +0300

    Handle change currency reducer

--- currency-converter/app/reducers/currencies.js
+++ currency-converter/app/reducers/currencies.js
@@ -1,14 +1,9 @@
 import {
   CHANGE_CURRENCY_AMOUNT,
   SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY,
 } from '../actions/currencies';
-
-// const initialState = {
-//   baseCurrency: 'USD',
-//   quoteCurrency: 'GBP',
-//   amount: 100,
-//   conversions: {},
-// };
 
 const initialState = {
   baseCurrency: 'USD',
@@ -56,17 +51,44 @@ const initialState = {
   },
 };
 
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: '',
+    rates: {},
+  };
+
+  if (state.conversions[action.currency]) {
+    conversion = state.conversions[action.currency];
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversion,
+  };
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
-      return { ...state,
-        amount: action.amount || 0,
-      };
+      return { ...state, amount: action.amount || 0 };
     case SWAP_CURRENCY:
       return {
         ...state,
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency,
+      };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action),
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action),
       };
     default:
       return state;
