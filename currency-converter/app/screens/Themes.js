97eeb5603b7a6de10ae5d4c19923fbc95f16db5d
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-09-15 22:54:54 +0300

    Setup theme actions

--- currency-converter/app/screens/Themes.js
+++ currency-converter/app/screens/Themes.js
@@ -1,23 +1,29 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { ScrollView, StatusBar } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
+import { changePrimaryColor } from '../actions/theme';
 
 const styles = EStyleSheet.create({
   $blue: '$primaryBlue',
+  $orange: '$primaryOrange',
   $green: '$primaryGreen',
   $purple: '$primaryPurple',
-  $orange: '$primaryOrange',
 });
 
 class Themes extends Component {
   static propTypes = {
     navigation: PropTypes.object,
-  }
+    dispatch: PropTypes.func,
+  };
 
-  handleThemePress = color => this.props.navigation.goBack()
+  handlePressTheme = (color) => {
+    this.props.dispatch(changePrimaryColor(color));
+    this.props.navigation.goBack();
+  };
 
   render() {
     return (
@@ -25,36 +31,33 @@ class Themes extends Component {
         <StatusBar translucent={false} barStyle="default" />
         <ListItem
           text="Blue"
-          onPress={() => this.handleThemePress(styles.$blue)}
-          checkmark={false}
+          onPress={() => this.handlePressTheme(styles.$blue)}
           selected
+          checkmark={false}
           iconBackground={styles.$blue}
         />
         <Separator />
-
         <ListItem
           text="Orange"
-          onPress={() => this.handleThemePress(styles.$orange)}
-          checkmark={false}
+          onPress={() => this.handlePressTheme(styles.$orange)}
           selected
+          checkmark={false}
           iconBackground={styles.$orange}
         />
         <Separator />
-
         <ListItem
           text="Green"
-          onPress={() => this.handleThemePress(styles.$green)}
-          checkmark={false}
+          onPress={() => this.handlePressTheme(styles.$green)}
           selected
+          checkmark={false}
           iconBackground={styles.$green}
         />
         <Separator />
-
         <ListItem
           text="Purple"
-          onPress={() => this.handleThemePress(styles.$purple)}
-          checkmark={false}
+          onPress={() => this.handlePressTheme(styles.$purple)}
           selected
+          checkmark={false}
           iconBackground={styles.$purple}
         />
         <Separator />
@@ -62,5 +65,4 @@ class Themes extends Component {
     );
   }
 }
-
-export default Themes;
+export default connect()(Themes);
