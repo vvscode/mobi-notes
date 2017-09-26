0adf776e1284d1b2ca3bc53a81947aadc3202179
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-09-26 07:30:24 +0300

    Fetch rates

--- currency-converter/app/config/sagas.js
+++ currency-converter/app/config/sagas.js
@@ -1,14 +1,35 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put, select } from 'redux-saga/effects';
 
 // 1, swap
 // 2, changed base currency
 // 3, initial
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import {
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  SWAP_CURRENCY,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
+
+export const getLatestRate = currency => fetch(`https://api.fixer.io/latest?base=${currency}`);
 
 const fetchLatestConversionRates = function* (action) {
-  console.log('TODO: Update the things.', action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRate, currency || 'USD');
+    const result = yield response.json();
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (error) {
+    yield put({ type: CONVERSION_ERROR, error: error.message });
+  }
 };
 
 const rootSaga = function* () {
