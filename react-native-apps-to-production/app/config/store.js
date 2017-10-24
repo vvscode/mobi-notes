6f1c570e5286beb83502ed373a699bcedcc63056
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-10-24 23:20:50 +0300

    persist data in async storage

--- react-native-apps-to-production/app/config/store.js
+++ react-native-apps-to-production/app/config/store.js
@@ -1,6 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { AsyncStorage as storage } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 import reducer from '../reducers';
 import rootSaga from './sagas';
@@ -11,7 +13,12 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(reducer, compose(applyMiddleware(...middleware), autoRehydrate()));
+
+persistStore(store, {
+  storage,
+  blacklist: ['network'],
+});
 
 sagaMiddleware.run(rootSaga);
 
