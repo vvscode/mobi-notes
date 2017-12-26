8341775ad86a69a5f4384bdafbecfc54e626f913
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-12-26 23:07:37 +0300

    Wrap all controls at Home into AnimateIn wrapper

--- react-native-apps-to-production/app/components/Animations/AnimateIn.js
+++ react-native-apps-to-production/app/components/Animations/AnimateIn.js
@@ -1,14 +1,15 @@
-import React, { Component, PropTypes } from "react";
-import { Animated, Dimensions } from "react-native";
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Animated, Dimensions } from 'react-native';
 
-const WINDOW_DIMENSIONS = Dimensions.get("window");
+const WINDOW_DIMENSIONS = Dimensions.get('window');
 
 class AnimateIn extends Component {
   static propTypes = {
     children: PropTypes.any,
     delay: PropTypes.number,
     duration: PropTypes.number,
-    type: PropTypes.oneOf(["fromBottom", "fromTop", "fadeIn"])
+    type: PropTypes.oneOf(['fromBottom', 'fromTop', 'fadeIn']),
   };
 
   constructor(props) {
@@ -21,46 +22,46 @@ class AnimateIn extends Component {
     Animated.timing(this.animatedValue, {
       toValue: 1,
       delay: this.props.delay || 0,
-      duration: this.props.duration || 500
+      duration: this.props.duration || 500,
     }).start();
   }
 
   render() {
     const { type } = this.props;
     let styles = {};
-    if (type === "fadeIn") {
+    if (type === 'fadeIn') {
       styles = {
         opacity: this.animatedValue,
-        transform: [{ scale: this.animatedValue }]
+        transform: [{ scale: this.animatedValue }],
       };
-    } else if (type === "fromTop") {
+    } else if (type === 'fromTop') {
       styles = {
         opacity: this.animatedValue,
         transform: [
           {
             translateY: this.animatedValue.interpolate({
               inputRange: [0, 1],
-              outputRange: [-WINDOW_DIMENSIONS.height, 0]
-            })
-          }
-        ]
+              outputRange: [-WINDOW_DIMENSIONS.height, 0],
+            }),
+          },
+        ],
       };
-    } else if (type === "fromBottom") {
+    } else if (type === 'fromBottom') {
       styles = {
         opacity: this.animatedValue,
         transform: [
           {
             translateY: this.animatedValue.interpolate({
               inputRange: [0, 1],
-              outputRange: [WINDOW_DIMENSIONS.height, 0]
-            })
-          }
-        ]
+              outputRange: [WINDOW_DIMENSIONS.height, 0],
+            }),
+          },
+        ],
       };
     }
 
     return (
-      <Animated.View style={[{ width: "100%", alignSelf: "center" }, styles]}>
+      <Animated.View style={[{ width: '100%', alignSelf: 'center' }, styles]}>
         {this.props.children}
       </Animated.View>
     );
