475e6ece42b3a95ef81c97ba277beea7e6eb9b88
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-01-10 16:58:27 +0300

    Test user interaction

---
+++ react-native-apps-to-production/app/components/__tests__/Button.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "react-dom";
+import { TouchableOpacity } from "react-native";
+import { shallow } from "enzyme";
+import sinon from "sinon";
+
+import { ClearButton, styles } from "../Button";
+
+it("renders successfully", () => {
+  const rendered = renderer.create(<ClearButton />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it("styles is an object", () => {
+  expect(typeof styles).toBe("object");
+});
+
+it("renders custom text pass via props", () => {
+  const rendered = renderer
+    .create(<ClearButton text="hey this is a test" />)
+    .toJSON();
+  expect(rendered).toMatchSnapshot();
+});
+
+it("handles a press", () => {
+  const callback = sinon.spy();
+  const wrapper = shallow(<ClearButton text="test 1" onPress={callback} />);
+
+  expect(wrapper.find(TouchableOpacity).length).toBe(1);
+  wrapper
+    .find(TouchableOpacity)
+    .first()
+    .props()
+    .onPress();
+
+  expect(callback.called).toBe(true);
+});
