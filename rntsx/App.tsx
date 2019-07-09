c16c4bc23027d027503e6e52018b52dae295bbc8
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-07-09 12:03:19 +0400

    Add button with alert

--- rntsx/App.tsx
+++ rntsx/App.tsx
@@ -1,30 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Button, Alert } from 'react-native';
 
 const App = () => {
-   const [text, setText] = useState('');
-    return <View style={styles.container}>
-      <Text>Enter text:</Text>
-      <TextInput 
-        value={text} 
-        onChangeText={setText} 
-        style={styles.textInput} 
-        defaultValue="ho-ho-ho" />
-      <Text>{text}</Text>
+  const [text, setText] = useState('');
+  return <View style={styles.container}>
+    <View style={styles.inputContainer}>
+
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        style={styles.textInput}
+        placeholder="An awesome input..."
+        placeholderTextColor="red" />
+      <Button
+        style={styles.button}
+        title="Click me"
+        onPress={() => Alert.alert(text)}
+      />
     </View>
+    <Text>{text}</Text>
+  </View>
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    // flex: 1,
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: 'lightgray',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   textInput: {
+    width: '70%',
     borderWidth: 1,
     borderColor: 'black',
-    width: 100,
+  },
+  button: {
+    width: '25%',
   }
 });
 
